c59bf524de4f12b873cb1160ffe9107a74bb06f3
James Thomson <101159512+JimmieThomson@users.noreply.github.com>
2025-03-24 04:14:47 +0000

    Fixed python changes

--- tutorial/src/bandit_tutorials/src/cmd_vel_publish.py
+++ tutorial/src/bandit_tutorials/src/cmd_vel_publish.py
@@ -6,14 +6,17 @@ from geometry_msgs.msg import Twist
 class BanditTutorial:
     def __init__(self): 
         self.movebase = rospy.Publisher('/mobile_base_controller/cmd_vel', Twist, queue_size=1)
-    
+        self.current_twist = Twist()
+        self.rate = rospy.Rate(10)  # 10Hz
+        
     # publishes velocity commands based on Twist
     def publish_velocity_loop(self):
-        while not rospy.is_shutdown:
-            self.current_twist.linear.x = 0
-            self.current_twist.linear.y = 0
-            self.current_twist.linear.z = -0.3
+        while not rospy.is_shutdown():
+            self.current_twist.linear.x = 0 # move forward and back
+            self.current_twist.linear.y = 0 # move left to right
+            self.current_twist.angular.z = 1.0
             self.movebase.publish(self.current_twist)
+            self.rate.sleep()
 
 if __name__ == "__main__":
     try:
